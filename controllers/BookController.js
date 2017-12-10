3a54cf60e85fcfc030fb95d9a052919024569ca0
Ikem Okonkwo <ikem.okonkwo@andela.com>
2017-12-10 11:26:10 +0100

    feat(app): enable pagination for /books endpoint

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,6 +1,21 @@
 export async function listBooks(ctx) {
-  const books = await ctx.db.Book.find({})
-  ctx.body = books
+  let { page, per_page, ...whereQuery } = ctx.query
+  const currentPage = Number(page)
+  const perPage = Number(per_page)
+  const offset = (currentPage - 1) * perPage
+
+  const query = () => ctx.db.Book.find(whereQuery)
+  const total = await query().count()
+  const books = await query()
+    .skip(offset)
+    .limit(perPage)
+
+  ctx.body = {
+    data: books,
+    total,
+    per_page: perPage,
+    page: page,
+  }
 }
 
 export async function storeBook(ctx) {
--- start/app.js
+++ start/app.js
@@ -1,18 +1,26 @@
 import Koa from 'koa'
+import joiSwagger from 'koa-joi-swagger'
+import { toSwaggerDoc } from './docs/utils'
+import docs from './docs'
 import router from './routes'
 import middlewares from './middlewares'
 import loadModels from '../lib/loadModels'
 import config from '../config'
 
+const { ui } = joiSwagger
+const swaggerDoc = toSwaggerDoc(docs)
+
 const app = async () => {
   const koa = new Koa()
   koa.context.db = await loadModels(config.database)
+  // mount swagger ui in `/swagger`
+  koa.use(ui(swaggerDoc, { pathRoot: '/docs', v3: true }))
   koa.use(middlewares())
   koa.use(router.routes())
   koa.use(router.allowedMethods())
 
   koa.on('error', (err, ctx) => {
-    ctx.logger.error('server error', err, ctx)
+    ctx.logger.error(err, 'Server Error')
   })
   koa.listen(config.server.port, config.server.host)
 }
